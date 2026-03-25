fefde59748cb9ead7748660c88d20fbbdf6950ad
Precious  Chuckson <sublime@Preciouss-MacBook-Air.local>
2026-03-25 14:01:15 +0100

    Added stats section with animation

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.count');
+
+counters.forEach(counter => {
+  counter.innerText = '0';
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const increment = target / 100;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCounter, 20);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
